326abe71b144b3d9966423a82dc91243f0bd22e4
Eduardo <martinsonsecaeduardo@gmail.com>
2025-02-23 13:43:26 +0100

    DIA 5

---
+++ 01-Python/Conceptos_Basicos/014_Modulos.py
@@ -0,0 +1,15 @@
+#Modulos
+
+import my_module
+
+my_module.sum_values(5,3,1)
+my_module.print_value("Hola Python")
+
+import math
+
+print(math.pi)
+print(math.pow(2,8))
+
+from math import pi as PI_VALUE
+
+print(PI_VALUE)
---
+++ 01-Python/Conceptos_Basicos/my_module.py
@@ -0,0 +1,8 @@
+def sum_values(number_one, number_two,nunmer_three) :
+    print(number_one + number_two +nunmer_three)
+
+def sum_two_values (first_number, second_number):
+    print(first_number + second_number)
+
+def print_value(value):
+    print(value)
